58105d272ecdb1014219dbb690c525d62d5a67da
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:18:01 +0000

    Add project files.

---
+++ CheckSubstringWithinString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckSubstringWithinString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool CheckSubstringWithinString(string substring, string fullString)
+        {
+            return fullString.Contains(substring);
+        }
+    }
+}
